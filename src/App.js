896324994f614f5ffe912f41ba5d8e9edd801df4
nejen-rajenteran <nejenrajenteran@gmail.com>
2022-09-21 21:14:03 -0400

    Version 1 Final

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import { useState, useEffect } from 'react';
+import { Component } from 'react';
 
 import SearchBox from './components/search-box/search-box.component'
 import CardList from './components/card-list/card-list.component'
 import './App.css';
 
-const App = () => {
-  const [searchField, setSearchField] = useState(''); // [value. setValue]
-  const [monsters, setMonsters] = useState([]);
-
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => setMonsters(users)
-  );
-
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  }
-  
-  return (
-    <div className="App">
-      <h1 className="app-title">Monsters Roledex</h1>
-
-      <SearchBox 
-        className='monsters-search-box'
-        onChangeHandler={onSearchChange} 
-        placeholder='Search Monsters' 
-      />
-
-      <CardList monsters={monsters} />
-    </div>
-  )
-}
-
-/*
 class App extends Component {
   constructor(){
     super();
@@ -85,6 +55,5 @@ class App extends Component {
     );
   }
 }
-*/
 
 export default App;
